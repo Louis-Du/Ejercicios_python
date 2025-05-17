64569a6af9f91288e9526eef5728ae0d3eca815f
Louis-Du <luis.a.d.f.beto@gmail.com>
2025-05-16 19:50:12 -0500

    Agrego un ejercico de bucles como prueba de vinculación de documento en un repositorio local a uno remoto.

---
+++ Ejercicios/04 Bucles /Ejercicio 1.py
@@ -0,0 +1,16 @@
+import random
+
+random.randing(1, 5)
+
+cuenta = 1
+
+while True:
+  numero = int(input("Introduce un numero: "))
+  
+  if numero != random.randing:
+    print("Sigue intentando")
+    
+  else:
+    print("Lo lograste")
+    
+    break
